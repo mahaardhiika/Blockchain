23df1765392dc1d54117674c02c8590f1a22d577
alkarobots <alkarobots@gmail.com>
2023-08-14 14:54:39 +0700

    hello ethernaut 0

--- Coding/Ethernaut/0_helloEthernaut.sol
+++ Coding/Ethernaut/0_helloEthernaut.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.0;
+pragma solidity ^0.8.0; // On prod, it is not good to use ^, it should be only the exact solidity version. 
 
 contract Instance {
 
@@ -8,7 +8,7 @@ contract Instance {
   string public theMethodName = 'The method name is method7123949.';
   bool private cleared = false;
 
-  // constructor
+  // constructor = state variable executed every time contract initialized
   constructor(string memory _password) {
     password = _password;
   }
@@ -20,7 +20,8 @@ contract Instance {
   function info1() public pure returns (string memory) {
     return 'Try info2(), but with "hello" as a parameter.';
   }
-
+ 
+  // if statement compare the info2 param, if == hello, it is ok. 
   function info2(string memory param) public pure returns (string memory) {
     if(keccak256(abi.encodePacked(param)) == keccak256(abi.encodePacked('hello'))) {
       return 'The property infoNum holds the number of the next info method to call.';
@@ -36,6 +37,7 @@ contract Instance {
     return 'If you know the password, submit it to authenticate().';
   }
 
+  // authenticate function, compare hashed password
   function authenticate(string memory passkey) public {
     if(keccak256(abi.encodePacked(passkey)) == keccak256(abi.encodePacked(password))) {
       cleared = true;
